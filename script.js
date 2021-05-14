ae13b6f5c3368ae42dab0f5758f71374b2b4dd17
Mahmoud Sabbagh <mahmoudsabbagh8@gmail.com>
2021-05-14 14:57:33 -0400

    Update script.js
    Hid API key

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var city = "Beirut"
 
 
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=98fdfd73f008f4270aeb1a4273e447f0' , function(data){
-    
+$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=apikey', function (data) {
+
     console.log(data)
     var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 
@@ -12,7 +12,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imper
 
     var location = data.name;
 
-    $('.icon').attr('src' , icon);
+    $('.icon').attr('src', icon);
     $('.weather').append(weather);
     $('.temp').append(temp);
     $('.location').append(location);
